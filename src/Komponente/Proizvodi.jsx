42b9e1fcfa6dfb624913abb9a163a16a760ae081
= <=nikolakadic1@gmail.com>
2024-06-22 23:15:37 +0200

    Pravljenje stranica za kartice

--- src/Komponente/Proizvodi.jsx
+++ src/Komponente/Proizvodi.jsx
@@ -2,6 +2,11 @@ import "../css/Proizvodi.css";
 import { useState } from "react";
 
 export default function Proizvodi() {
+  const [stranica, setStranica] = useState(1);
+  const [broj, setBroj] = useState(10);
+  console.log(stranica);
+  const niz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
   return (
     <div className="Proizvodi">
       <div className="Filter">
@@ -10,15 +15,27 @@ export default function Proizvodi() {
         <div>Marka</div>
       </div>
       <div className="Krt">
-        <Kartica />
-        <Kartica />
-        <Kartica />
-        <Kartica />
+        {niz.slice((stranica - 1) * 10, stranica * 10).map((it) => (
+          <Kartica />
+        ))}
+      </div>
+      <div className="Str">
+        {[...Array(Math.ceil(niz.length / 10)).keys()]
+          .map((i) => i + 1)
+          .map((it) => (
+            <Stranice broj={it} setStr={setStranica} />
+          ))}
       </div>
     </div>
   );
 }
-
+function Stranice({ broj, setStr }) {
+  return (
+    <div className="Stranica" onClick={() => setStr(broj)}>
+      {broj}
+    </div>
+  );
+}
 function Pretraga() {
   const [pretraga, setPretraga] = useState("");
   return <input type="text" onChange={(e) => setPretraga(e.target.value)} />;
